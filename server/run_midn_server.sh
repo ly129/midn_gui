b3f3d69a919355c4ded91fa0843aa6da1e1be0de
Luyaochen1 <lychen9999@gmail.com>
2022-07-15 09:23:18 -0500

    Update run_midn_server.sh

--- server/run_midn_server.sh
+++ server/run_midn_server.sh
@@ -4,7 +4,7 @@ cd  /MIDN_Server
 ./uwsgi_start.sh
 
 cd /MIDN_Server/streamlit
-nohup streamlit run midn_server_st.py  --server.port=8881 &
+nohup streamlit run midn_central_site_st.py  --server.port=8881 &
 
 
 /bin/bash
