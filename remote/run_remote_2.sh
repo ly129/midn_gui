6b604dc568798e30c13fe2c2c0b77ea506742347
Luyaochen1 <lychen9999@gmail.com>
2022-07-15 12:28:16 -0500

    Update run_remote_2.sh

--- remote/run_remote_2.sh
+++ remote/run_remote_2.sh
@@ -1,1 +1,1 @@
-sudo docker run  -it -d -p 7605:80  -p 6000:6000 --name=midn_remote_2 luyaochen/midn_remote
+sudo docker run  -it -d -p 7605:80  -p 6001:6000 --name=midn_remote_2 luyaochen/midn_remote
